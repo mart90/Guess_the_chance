9961c5da1ef2d2803d9c30979250e4fe9ada26d5
Mart Broekkamp <martbr90@gmail.com>
2024-08-28 09:00:41 +0200

    discord notifs

--- backend/discord_notifier.py
+++ backend/discord_notifier.py
@@ -1,18 +1,32 @@
 import requests
 import json
+from mysql import *
 
 
-class DiscordNotifier:
-    def __init__(self):
-        self.username = "NotificationBot"
-        self.notify_webhook = "https://discord.com/api/webhooks/1278236084945551423/UAPZzNhlEhEqg3686_SiCf26WRqC4S9vGBN_GuFVjjycehOI64XUsYPqXWVsTZvbpEGf"
-    
-    def message(self, message):
-        data = {
-            "content": message,
-            "username": self.username
-        }
-        requests.post(self.notify_webhook, data=json.dumps(data), headers={"Content-Type": "application/json"})
+username = "Notification bot"
+notify_webhook = "https://discord.com/api/webhooks/1278236084945551423/UAPZzNhlEhEqg3686_SiCf26WRqC4S9vGBN_GuFVjjycehOI64XUsYPqXWVsTZvbpEGf"
 
+mysql = MySQL().connect(mysql_ip, mysql_db)
 
-discord_notifier = DiscordNotifier()
+mysql.query("""
+    SELECT 
+        e.id,
+        e.description,
+        ec.discord_role_id 
+    FROM event e 
+    JOIN event_category ec ON e.category_id = ec.id
+    WHERE e.date_close <= (CURRENT_TIMESTAMP + INTERVAL 28 HOUR)""")
+
+result = mysql.cursor.fetchall()
+mysql.commit_and_close()
+
+for row in result:
+    event_name = row[1]
+    category_role_id = row[2]
+    link = f"https://guessthechance.com/events/{row[0]}"
+
+    data = {
+        "content": f"Guessing for event \"{event_name}\" closes soon. {link} <@&{category_role_id}>",
+        "username": username
+    }
+    requests.post(notify_webhook, data=json.dumps(data), headers={"Content-Type": "application/json"})
